fefcd602ccd716280a57df2102d028a9edc390f1
spyllum <topoglumetehan1@gmail.com>
2024-11-28 22:06:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import random
+degıskenler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+uzunluk = int(input("Lütfen şifre uzunluğunu giriniz : "))
+sifre  = ""
+for i in range(uzunluk):
+    sifre += random.choice(degıskenler)
 
+print("Oluşturla Şİfre : ", sifre)
